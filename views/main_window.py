4a1b4f86d33d5e93f8e9a15da35708f6cd0d6361
Rofernan1205 <rodrigo.1997.fm@gmail.com>
2025-09-30 16:13:22 -0500

    boxes

--- views/main_window.py
+++ views/main_window.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from email.header import Header
 
@@ -24,14 +25,43 @@ class DashboardApp(BaseWindow):
         self.setStyleSheet(GRADIENT_GLOBAL)
 
         # Layout Principal: Horizontal (Sidebar | Contenido)
-        self.main_h_layout = QHBoxLayout(main_widget)
+        self.main_h_layout = QVBoxLayout(main_widget)
         self.main_h_layout.setContentsMargins(0, 0, 0, 0)
         self.main_h_layout.setSpacing(0)
 
-        header = QWidget()
-        header.setStyleSheet("border-bottom: 2px solid white; background-color:red;")
+        sidebar = QFrame()
+        sidebar.setStyleSheet("background-color: transparent;")
+        layout_sidebar = QHBoxLayout(sidebar)
+        layout_sidebar.setContentsMargins(0,0,0,0)
+        layout_sidebar.setSpacing(0)
 
-        self.main_h_layout.addWidget(header)
+        box_logo = QFrame()
+        box_logo.setStyleSheet("background-color: red;")
+
+        box_title = QFrame()
+        box_title.setStyleSheet("background-color: green;")
+
+        box_profile = QFrame()
+        box_profile.setStyleSheet("background-color: blue;")
+
+
+        layout_sidebar.addWidget(box_logo,2)
+        layout_sidebar.addWidget(box_title,4)
+        layout_sidebar.addWidget(box_profile, 4)
+
+
+
+
+
+
+
+
+        content = QFrame()
+        content.setStyleSheet("background-color: transparent; border-top: 1px solid #b3b5b9;")
+
+        # Sidebar 20% - Content 80%
+        self.main_h_layout.addWidget(sidebar, 1)
+        self.main_h_layout.addWidget(content, 9)
 
 
 if __name__ == "__main__":
@@ -41,3 +71,22 @@ if __name__ == "__main__":
     sys.exit(app.exec())
 
 
+
+        # box_logo = QFrame()
+        # box_logo.setStyleSheet("backbround-color: red;")
+        # layout_sidebar.addWidget(box_logo)
+        #
+        # ruta = os.getcwd()
+        # nueva_ruta = ruta[:len(ruta)-6]
+        # logo_pixmap = QPixmap(os.path.join(nueva_ruta, r"assets\images\apubyte.jpg"))
+        #
+        # if logo_pixmap.isNull():
+        #     print("¡ERROR! No se pudo cargar la imagen. Revisa la ruta.")
+        # else:
+        #     print("Imagen cargada exitosamente.")
+        #
+        # label = QLabel()
+        # label.setPixmap(logo_pixmap)
+        # label.setScaledContents(True)  # Hace que la imagen se ajuste al tamaño del QLabel
+        # label.resize(300, 200)
+        # box_logo.addWidget(label)
